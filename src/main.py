701321d677340002ee0261c0ec80167834946f8d
SharonFernando <sharon_fernando@hotmail.com>
2026-09-11 14:48:43 -0300

    Atualização main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from transform import (
-    get_info, 
+    get_info,
     msg,
     create_game,
     create_system,
@@ -18,49 +18,45 @@ from config import (LIST, STATS, DB_TABLES)
 gameList = pd.read_csv(LIST)
 statsList = pd.read_csv(STATS)
 
-gameList = gameList[
-    [
-    "id",
-    "playcount",
-    "gametime",
-    "lastplayed",
-    "releasedate",
-    "rom"
-    ]
-]
+roms = gameList[["id","rom"]].dropna(subset=['id']).drop_duplicates(subset=['id'])
 
 msg("⚡ Fazendo requisição para a API...")
 msg(f"🎮 Buscando dados para {len(gameList)} jogos...")
 
 jogo = get_info(122976)
 
-df_jogos = pd.DataFrame(jogo,index=[0])
+df_games = pd.DataFrame(jogo,index=[0])
+roms['id'] = roms['id'].astype(int)
+df_games['gameId'] = df_games['gameId'].astype(int)
+df_games = df_games.merge(roms, left_on='gameId', right_on='id', how='left')
 
-game = create_game(df_jogos)
+print(df_games)
+
+game = create_game(df_games)
 print(game)
 
-systems = create_system(df_jogos)
+systems = create_system(df_games)
 print(systems)
 
 stats = create_stats(statsList)
 print(stats)
 
-genre = create_genre(df_jogos)
+genre = create_genre(df_games)
 print(genre)
 
-developer = create_developer(df_jogos)
+developer = create_developer(df_games)
 print(developer)
 
-publisher = create_publisher(df_jogos)
+publisher = create_publisher(df_games)
 print(publisher)
 
-region = create_region(df_jogos)
+region = create_region(df_games)
 print(region)
 
-rom = create_rom(gameList)
+rom = create_rom(df_games)
 print(rom)
 
-synopsis = create_synopsis(df_jogos)
+synopsis = create_synopsis(df_games)
 print(synopsis)
 
 msg("✅ Concluído.")
